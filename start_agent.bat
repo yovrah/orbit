1f11710cb884f4c6fd35388b820690526781fa58
yovrah <yovrah@users.noreply.github.com>
2026-06-13 06:37:02 +0200

    fix: correct cmd syntax in start_agent.bat

--- start_agent.bat
+++ start_agent.bat
@@ -2,11 +2,6 @@
 title Orbit Remote Agent
 echo Starting Orbit Remote Agent...
 cd /d "%~dp0agent"
-if not exist "venv\Scripts\activate.bat" (
-    echo Error: Virtual environment (venv) not found in %CD%
-    pause
-    exit /b
-)
 call venv\Scripts\activate.bat
 python src/main.py
 pause
